dd4db6adce2ad1fb61762bb2f1dac67e585a2a79
Bakary91 <bakarysoumare259@gmail.com>
2026-06-27 05:26:27 -0400

    feat(frontend): ajout route donation et react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,16 @@
-// import HomePage from "./components/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
 import RegisterPage from "./components/RegisterPage";
+import DonationPage from "./components/DonationPage";
 
 export default function App() {
-  return <RegisterPage />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/donation" element={<DonationPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
